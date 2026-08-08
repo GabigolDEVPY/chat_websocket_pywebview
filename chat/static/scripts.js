468c1d730aabaebf98de27f7511414fb4fd39b19
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-08-08 14:06:47 -0300

    arrumando logica de recebimentos

--- chat/static/scripts.js
+++ chat/static/scripts.js
@@ -17,6 +17,15 @@ function OpenSocket (room) {
 
     ws.onopen = async () => {
         document.getElementById("messages").innerHTML = "";
+        const footerEl = `
+            <div class="input-group">
+                <input id="messageInput" type="text" class="form-control rounded-pill border-secondary-subtle" placeholder="Digite uma mensagem..." aria-label="Mensagem">
+                <button class="btn btn-primary rounded-circle ms-2 d-flex align-items-center justify-content-center" onclick="SendMessage()" id="sendButton" style="width:45px;height:45px;" title="Enviar mensagem">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16"><path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/></svg>
+                </button>
+            </div>
+        `
+        document.getElementById("footer-message").innerHTML = footerEl;
     }
 
 
--- friend/views.py
+++ friend/views.py
@@ -1,11 +1,9 @@
 from django.views.generic import TemplateView, View
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import JsonResponse, response
 from django.contrib.auth.models import User
 from django.shortcuts import render
-from .models import RequestFriend
+from .models import RequestFriend, Friend
 from .services.friend_service import FriendService
+
 
 
 class SearchFriendView(View):
@@ -13,8 +11,7 @@ class SearchFriendView(View):
         self.username = request.GET.get("q")
         user = User.objects.filter(username=self.username).exclude(id=request.user.id).first()
         request_friend = RequestFriend.objects.filter(friend=user).first()
-        context = {"user": user, "request_friend": request_friend}
-        print(context)
+        context = {"user": user, "request_friend": request_friend, "is_friend": Friend.objects.filter(user=request.user, friend=user).exists()}
         return render(request, template_name="partials/list_friend_result.html", context=context)
     
 
