5afc769d40bbf5f66141f8d624bbc874a114f0e8
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-08-10 18:56:05 -0300

    arrumando arquivos

--- chat/static/scripts.js
+++ chat/static/scripts.js
@@ -2,12 +2,15 @@
 
 let ws = null;
 const user = JSON.parse(document.getElementById("current-user").textContent);
+const friendSelected = null;
 
 
-function OpenSocket (room) {
+function OpenSocket (room, friend_username) {
+    document.getElementById("friend-name-id").textContent = friend_username;
     if (ws) {
         ws.close()
     }
+
 
     const protocol = window.location.protocol === "https:" ? "wss" : "ws";
 
@@ -17,15 +20,8 @@ function OpenSocket (room) {
 
     ws.onopen = async () => {
         document.getElementById("messages").innerHTML = "";
-        const footerEl = `
-            <div class="input-group">
-                <input id="messageInput" type="text" class="form-control rounded-pill border-secondary-subtle" placeholder="Digite uma mensagem..." aria-label="Mensagem">
-                <button class="btn btn-primary rounded-circle ms-2 d-flex align-items-center justify-content-center" onclick="SendMessage()" id="sendButton" style="width:45px;height:45px;" title="Enviar mensagem">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16"><path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/></svg>
-                </button>
-            </div>
-        `
-        document.getElementById("footer-message").innerHTML = footerEl;
+        document.getElementById("footer-message").style.display = "block";
+        document.getElementById("friend-name-id").style.display = "block";
     }
 
 
